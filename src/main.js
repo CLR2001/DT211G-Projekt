3b9aa8d32d0ba7e232572f420cfc1753a50ac244
CLR2001 <luro2501@student.miun.se>
2026-03-09 14:06:17 +0100

    removed redundant files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 "use strict";
 import './scss/main.scss';
-import { initNavigation } from './js/navigation';
 import { initPageHandler } from './js/page-handler.js';
 import { initThemeHandler } from './js/theme-handler.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  initNavigation();
   initPageHandler();
   initThemeHandler();
 });
