9e1f23a70f1923f0f485c5f1e0374b385a28dc9a
CLR2001 <luro2501@student.miun.se>
2026-03-11 00:59:21 +0100

    added maps for each race and a search function

--- src/js/pages/index-page.js
+++ src/js/pages/index-page.js
@@ -6,6 +6,10 @@ export const indexPage = `
     <h1>Upplev F1 på plats. Planera din resa idag</h1>
     ${getOptimizedImage(heroSrc, {alt: "Red Bull Formula 1 car during Singapore GP 2025", layout: "full", lazy: false, priority: true, className: "hero-image"})}
   </section>
+  <form class="search-input">
+      <label for="search-input">Sök:</label>
+      <input type="text" name="search" id="search-input" placeholder="Sök efter land eller bana" autocomplete="off">
+    </form>
   <section class="race-section">
     
   </section>
--- src/js/race-data-handler.js
+++ src/js/race-data-handler.js
@@ -1,8 +1,9 @@
 async function writeRaceGrid(dataArray) {
   const raceSection = document.querySelector('.race-section')
-
-  const races = await fetchRaceData();
-  races.forEach(race => {
+  raceSection.innerHTML = "";
+  /* ------------------------- Loops thorugh all races ------------------------ */
+  let loopCounter = 1;
+  dataArray.forEach(race => {
     const container = document.createElement('section');
     const contentHeader = document.createElement('div');
     const contentBody = document.createElement('div');
@@ -20,7 +21,13 @@ async function writeRaceGrid(dataArray) {
 
     /* --------------------- Creates h2-tag for country name -------------------- */
     const countryName = document.createElement('h2');
-    countryName.innerHTML = race.Circuit.Location.country;
+    const country = race.Circuit.Location.country;
+    if (country === 'USA' || country === 'UAE' || country === 'Spain' && race.Circuit.Location.locality === 'Barcelona') {
+      countryName.innerHTML = race.raceName.replaceAll(' Grand Prix',"");
+    }
+    else {
+      countryName.innerHTML = race.Circuit.Location.country;
+    }
 
     /* ------------------ Creates img-tag for country flag ------------------ */
     const raceCountry = race.Circuit.Location.country;
@@ -55,8 +62,32 @@ async function writeRaceGrid(dataArray) {
       contentHeader.append(raceDate);
     }
 
+    /* ------------------------------- Creates map ------------------------------ */
+    const mapContainer = document.createElement('div');
+    mapContainer.id = `map-${loopCounter}`;
+    mapContainer.classList.add('race-map');
+    contentBody.append(mapContainer);
+
+    /* --------------- Creates link to google maps for directions --------------- */
+    const directionsButton = document.createElement('a');
+    directionsButton.classList.add('directions-button');
+    directionsButton.innerHTML = 'Få vägbeskrivning';
+    const lat = race.Circuit.Location.lat;
+    const lon = race.Circuit.Location.long;
+    directionsButton.href = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lon}`;
+    directionsButton.target = '_blank'
+    contentBody.append(directionsButton);
+    
+    
+
+
+
+
+
     container.append(contentHeader, contentBody);
-    raceSection.append(container)
+    raceSection.append(container);
+    createRaceBodyContent(race, loopCounter);
+    loopCounter++;
   });
 }
 
@@ -76,8 +107,42 @@ async function fetchRaceData() {
   }
 }
 
+/* ------------------ Function to create race-content-body ------------------ */
+function createRaceBodyContent(race, id) {
+  const lat = parseFloat(race.Circuit.Location.lat);
+  const lon = parseFloat(race.Circuit.Location.long);
+  var map = L.map(`map-${id}`).setView([lat, lon], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  const marker = L.marker([lat, lon]).addTo(map);
+  marker.bindPopup(`<b>${race.Circuit.circuitName}</b>`);
+}
+
+/* -------------- Function to filter races with a search input -------------- */
+function searchData(dataArray) {
+  const input = document.querySelector("#search-input").value;
+  const data = dataArray.filter(race => 
+    race.Circuit.Location.country.toLowerCase().includes(input.toLowerCase()) ||
+    race.raceName.toLowerCase().includes(input.toLowerCase()) || 
+    race.Circuit.circuitName.toLowerCase().includes(input.toLowerCase())
+  );
+  const filteredRaceData = [...data];
+  writeRaceGrid(filteredRaceData);
+}
+
 /* ----------------------------- Exports module ----------------------------- */
-export function initRaceData() {
-  writeRaceGrid();
+export async function initRaceData() {
+  const raceData = await fetchRaceData();
+  writeRaceGrid(raceData);
+  const searchInput = document.querySelector("#search-input");
+  console.log(searchInput);
+  
+  searchInput.addEventListener('input', () => {
+    searchData(raceData);
+  });
 }
 
